edb827e128279bb42609957c354764ff445b59d1
David Liman <limanoit@gmail.com>
2023-08-26 09:09:18 -0700

    update faq

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <section>
-    Home-based Pet Grooming Service  • 16561 China Berry Ct, Chino Hills, CA • (840) 212-9605
+    Home-based Pet Grooming Service • 16561 China Berry Ct, Chino Hills, CA • (840) 212-9605 • Instagram • Yelp • Whatsapp
 </section>
 
 <nav>
     <a href="/">Home</a>
     <a href="/services">Services</a>
     <a href="/policies">Policies</a>
-    <a href="/instagram">Instagram</a>
+    <a href="/faq">F.A.Q</a>
     <a href="/contact-us">Contact Us</a>
 </nav>
 

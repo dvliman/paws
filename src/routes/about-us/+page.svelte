18217f3eab39d44b9f5f131e2bc76498850f732e
David Liman <limanoit@gmail.com>
2023-08-28 20:40:02 -0700

    add fiona

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Fiona from '$lib/assets/about/fiona.jpeg'
+</script>
 <main>
     <h1>About Us</h1>
     <p>
@@ -5,18 +8,22 @@
     </p>
     <h2>Meet our Team</h2>
     <strong>Fiona L</strong>
+    <img src={Fiona} alt="Fiona">
     <p>My name Fiona L and I am your groomer</p>
     <br/>
 
     <strong>Tiny (@tinylittleyorkie)</strong>
+    <img src={Fiona} alt="Tiny">
     <p>Alerting of new occurennces & very supportive in every way. The bread & butter of the operation.</p>
     <br/>
 
     <strong>Taco</strong>
+    <img src={Fiona} alt="Taco">
     <p>Very experienced, full of energy, go-getter! Loves attention.</p>
     <br/>
 
     <strong>Nino</strong>
+    <img src={Fiona} alt="Nino">
     <p>Love to socialize whether he is invited or not</p>
     <br/>
 

cbc7f2010c0d4421c00b69fff010b24618b27661
David Liman <limanoit@gmail.com>
2023-09-18 12:42:23 -0700

    meta pixel code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,24 @@
+<!-- svelte-ignore a11y-missing-attribute -->
+<svelte:head>
+	<!-- Meta Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '826349535656951');
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=826349535656951&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Meta Pixel Code -->
+</svelte:head>
+
 <script>
 	import Analytics from '$lib/analytics.svelte';
 	import '../global.css';
@@ -213,3 +234,6 @@
 		display: none !important;
 	}
 </style>
+
+
+

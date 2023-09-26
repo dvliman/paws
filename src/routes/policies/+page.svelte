0e7b3ac1920ee69822a72ccbf46f16be047b74a8
David Liman <limanoit@gmail.com>
2023-09-26 09:29:51 -0700

    update policies to include dematting

--- src/routes/policies/+page.svelte
+++ src/routes/policies/+page.svelte
@@ -1,49 +1,50 @@
 <script>
-	import { MetaTags } from 'svelte-meta-tags';
+  import { MetaTags } from 'svelte-meta-tags';
 </script>
-	
+  
 <MetaTags title="Policies" description="Please read our policies before booking an appointment" />
 
 <section>
-	<h1>Our policies</h1>
-	<div>
-		<h3>Late</h3>
-		<p>Clients that are more than 20 minutes late may be required to cancel and/or reschedule.</p>
-	</div>
+  <h1>Our policies</h1>
+  <div>
+    <h3>Late</h3>
+    <p>Clients that are more than 20 minutes late may be required to cancel and/or reschedule.</p>
+  </div>
 
-	<div>
-		<h3>Cancelation fee – $20 per dog.</h3>
-		<p>We require a 24 hour notice to reschedule an appointment.</p>
-	</div>
+  <div>
+    <h3>Cancelation</h3>
+    <p>We require a 24 hour notice to reschedule an appointment.</p>
+  </div>
 
-	<div>
-		<h3>Dematting fee – $10+</h3>
-		<p>
-			If your pet requires extra brushing due to matting we may charge a fee for the extra time it
-			takes. Dematting can be a stressful and painful process, please brush your pet's hair daily to
-			avoid matted hair.
-		</p>
-	</div>
-	<div>
-		<h3>Special Handling fee – $10+</h3>
-		<p>Pet(s) that bite, act aggressive, or require extra assistance to stand. Please inform us if your pet might require special handling, in particular if your pet has a
-			history of biting or is aggressive towards other dogs.
-		</p>
-	</div>
+  <div>
+    <h3>Dematting fee ($10 / 15 mins)</h3>
+    <p>
+      If your pet requires extra brushing due to matting we may charge a fee for the extra time it
+      takes. Dematting can be a stressful and painful process, please brush your pet's hair daily to
+      avoid matted hair.
+    </p>
+  </div>
+
+  <div>
+    <h3>Special Handling</h3>
+    <p>Pet(s) that bite, act aggressive, or require extra assistance to stand. Please inform us if your pet might require special handling, in particular if your pet has a
+      history of biting or is aggressive towards other dogs.
+    </p>
+  </div>
 </section>
 
 <style>
-	h1 {
-		width: 100%;
-		text-align: center;
-	}
-	p {
-		margin: 0;
-	}
-	div {
-		padding: 2rem;
-		border: 1px solid #f2f2f2;
-		width: 100%;
-		border-radius: 1rem;
-	}
+  h1 {
+    width: 100%;
+    text-align: center;
+  }
+  p {
+    margin: 0;
+  }
+  div {
+    padding: 2rem;
+    border: 1px solid #f2f2f2;
+    width: 100%;
+    border-radius: 1rem;
+  }
 </style>

208b58ccbf3654031d2848ca887e5ce5c12828e4
David Liman <limanoit@gmail.com>
2023-09-26 19:37:25 -0700

    fix typo

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -24,7 +24,7 @@
 				<div class="price">$70</div>
 			</li>
 			<li>
-				<div><span>76 lbs 81 lbs & up up</span><span>XLarge dogs</span></div>
+				<div><span>76 lbs & up</span><span>XLarge dogs</span></div>
 				<div class="price">$75</div>
 			</li>
 		</ul>
@@ -49,7 +49,7 @@
 				<div class="price">$115</div>
 			</li>
 			<li>
-				<div><span>76 lbs 81 lbs & up up</span><span>XLarge dogs</span></div>
+				<div><span>76 lbs & up</span><span>XLarge dogs</span></div>
 				<div class="price">$145</div>
 			</li>
 		</ul>
